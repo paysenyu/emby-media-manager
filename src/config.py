caa3ebf9326db44a09ca1cb2484d0a3f9a677f64
paysen <peiheng.yu@outlook.com>
2026-03-06 10:28:37 +0800

    Add configuration management for Emby server, database, and application settings

---
+++ src/config.py
@@ -0,0 +1,17 @@
+# Configuration Management for Emby Server
+
+# Emby Server settings
+EMBY_SERVER_URL = 'http://localhost:8096'
+EMBY_SERVER_API_KEY = 'your_api_key_here'
+
+# Database settings
+DATABASE_HOST = 'localhost'
+DATABASE_PORT = 5432
+DATABASE_NAME = 'emby_db'
+DATABASE_USER = 'emby_user'
+DATABASE_PASSWORD = 'your_db_password_here'
+
+# Application settings
+APPLICATION_NAME = 'Emby Media Manager'
+APPLICATION_VERSION = '1.0.0'
+APPLICATION_LOG_LEVEL = 'INFO'
